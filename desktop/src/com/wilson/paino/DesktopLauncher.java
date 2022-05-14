8ee123ff4087ca91a3474ad8ac84cf11abed1088
Wilson He <wilson@Wilsons-MBP.fios-router.home>
2022-05-13 20:18:02 -0400

    First Commit

---
+++ desktop/src/com/wilson/paino/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.wilson.paino;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.wilson.paino.Paino;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Paino");
+		new Lwjgl3Application(new Paino(), config);
+	}
+}
